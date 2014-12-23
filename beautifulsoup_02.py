8081f2306729932aef2d01c803d23a894439d7e3
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-12-22 21:41:10 -0500

    Revisions

--- beautifulsoup_02.py
+++ beautifulsoup_02.py
@@ -1,15 +1,18 @@
 import urllib2
 from bs4 import BeautifulSoup
+"""Importing a speech"""
 
 URL = 'http://www.whitehouse.gov/net-neutrality'
 DATA = urllib2.urlopen(URL).read()
 HTML_SOUP = BeautifulSoup(DATA)
 
+
 def obama_net_neutrality():
-    links=soup.findAll('p','tt')
+    """Prints President Obama's speech on net neutrality"""
+    links = HTML_SOUP.findAll('tt')
     for link in links:
         print link
-
+        
 if __name__ == '__main__':
     # You can use this conditional block for debugging
     # print HTML_SOUP
--- beautifulsoup_03.py
+++ beautifulsoup_03.py
@@ -5,11 +5,18 @@ URL = 'http://sps.cuny.edu/whoweare/departments/it'
 DATA = urllib2.urlopen(URL).read()
 SOUP = BeautifulSoup(DATA)
 
-def sps_it_department:
+def sps_it_department():
     """Makes business cards out of SPS IT website"""
-    links = SOUP.findAll('<span', ('class':'name bold'))
-    for link in links:
-        print link
+    name = []
+    direct = {}
+    names = SOUP.findAll('span',{'class':'name bold'})
+     for link in names:
+         name.append(link.get_text())
+    for i in name:
+        last = i.split(",")[0].lstrip()
+        first = i.split(",")[1].rstrip()
+        direct[last] = first
+    print direct
         
 if __name__ == '__main__':
     # You can use this conditional block for debugging
