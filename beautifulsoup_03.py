ea24cdf8e07217f9f2590968c4288f9a501220b3
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-12-23 19:18:11 -0500

    Revision

--- beautifulsoup_03.py
+++ beautifulsoup_03.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Using bs4 part 2"""
+
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -5,17 +9,23 @@ URL = 'http://sps.cuny.edu/whoweare/departments/it'
 DATA = urllib2.urlopen(URL).read()
 SOUP = BeautifulSoup(DATA)
 
+
 def sps_it_department():
     """Makes business cards out of SPS IT website"""
     name = []
-    direct = {}
+    direct = []
     names = SOUP.findAll('span',{'class':'name bold'})
-     for link in names:
+    email = SOUP.select('a[href^=mailto]')
+    emails = []
+    for i in email:
+        if i.string != None:
+            emails.append(i.string.encode('utf-8').strip())
+    for link in names:
          name.append(link.get_text())
-    for i in name:
+    for j,i in enumerate(name):
         last = i.split(",")[0].lstrip()
         first = i.split(",")[1].rstrip()
-        direct[last] = first
+        direct.append({(last,first): emails[j]})
     print direct
         
 if __name__ == '__main__':
